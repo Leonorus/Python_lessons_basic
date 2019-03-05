97b3c33c8ee56d2124986ed3de8f60a9d32b9a70
fvysokov <eiW8mieb>
2019-03-05 20:32:48 +0300

    lesson 3 easy2 done. add tasks

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -1,16 +1,10 @@
+# EASY
+
 # Задание-1:
 # Напишите функцию, округляющую полученное произвольное десятичное число
 # до кол-ва знаков (кол-во знаков передается вторым аргументом).
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
-
-def my_round(number, ndigits):
-    pass
-
-
-print(my_round(2.1234567, 5))
-print(my_round(2.1999967, 5))
-print(my_round(2.9999967, 5))
 
 
 # Задание-2:
@@ -19,10 +13,18 @@ print(my_round(2.9999967, 5))
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
-def lucky_ticket(ticket_number):
-    pass
+def is_lucky_ticket(ticket):
+    ticket_number = [int(n) for n in ticket]
+    if sum(ticket_number[:3]) == sum(ticket_number[3:]):
+        return True
+    else:
+        return False
 
 
-print(lucky_ticket(123006))
-print(lucky_ticket(12321))
-print(lucky_ticket(436751))
+ticket = input("Ticket number: ")
+if is_lucky_ticket(ticket):
+    print("Билет счастливый")
+else:
+    print("Билет не является счастливым")
+
+
