f242132314ae7d20ba9c332b25f92d006cb4f4dd
fvysokov <eiW8mieb>
2019-03-14 15:09:20 +0300

    lesson 4 normal first done

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -1,10 +1,34 @@
-#NORMAL
+# NORMAL
 # Задание-1:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+import random
 
+file = r"testfile"
+huge_number = [str(random.randint(0, 9)) for i in range(0, 2501)]
+with open(file, "w") as f:
+    f.write("".join(huge_number))
+
+with open(file, "r") as f:
+    readed_number = f.read()
+
+prev_char = ""
+sequence = ""
+longest_sequence = ""
+for char in readed_number:
+    if char == prev_char:
+        sequence += char
+        if len(sequence) > len(longest_sequence):
+            longest_sequence = sequence
+    else:
+        sequence = char
+
+    prev_char = char
+
+print(longest_sequence)
 # Задание-2
-# Сформировать квадратную матрицу, в каждой строке которой находится ровно один ноль на случайном месте, остальные элементы тоже рандомные. Пользователь вводит размер
+# Сформировать квадратную матрицу, в каждой строке которой находится ровно один ноль на случайном месте, остальные элементы
+# тоже рандомные. Пользователь вводит размер
