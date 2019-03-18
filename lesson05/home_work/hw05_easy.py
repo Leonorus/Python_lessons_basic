2c3bf18af19288640f13a1ef22f0364fda3a971c
fvysokov <eiW8mieb>
2019-03-18 17:06:46 +0300

    lesson 5 easy done

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,34 @@
+# EASY
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+# Учесть проверку на то что папка не пуста и на то, что такая папка уже существует
+import os
+import re
+
+while True:
+    print("1. Create dirs\n" "2. Remove dirs\n" "3. Quit")
+    userComand = int(input("Enter action: "))
+
+    if userComand == 1:
+        for i in range(0, 10):
+            if not os._exists(f"dir_{i}"):
+                os.mkdir(f"dir_{i}")
+    elif userComand == 2:
+        for dir in os.listdir("."):
+            if re.match("dir_[0-9]", dir):
+                if not os.listdir(dir):
+                    os.rmdir(dir)
+    elif userComand == 3:
+        exit()
+    else:
+        print("Please input number of action")
+
 
 # Задача-2:
-# Напишите скрипт, отображающий папки текущей директории.
+# Напишите скрипт, который выводит в консоль список файлов и папок в указанной директории.
 
-# Задача-3:
-# Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+directory = input("Enter directory to list: ")
+for file in os.listdir(directory):
+    print(file)
